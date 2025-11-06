e7c64848130135e24195e55443cb8f05be72b48a
Cristian Osia <osia.developer@gmail.com>
2025-11-06 08:29:20 -0500

    Ultimo

--- app/src/main/java/com/coljuegos/sivo/ui/establecimiento/resumen/ResumenInventarioViewModel.kt
+++ app/src/main/java/com/coljuegos/sivo/ui/establecimiento/resumen/ResumenInventarioViewModel.kt
@@ -3,7 +3,9 @@ package com.coljuegos.sivo.ui.establecimiento.resumen
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 
+import javax.inject.Inject
+
 @HiltViewModel
-class ResumenInventarioViewModel : ViewModel() {
+class ResumenInventarioViewModel @Inject constructor() : ViewModel() {
     // TODO: Implement the ViewModel
 }
